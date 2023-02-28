74f15c18817da7bcd1d759ee0419e6c8c1320eca
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-02-27 22:41:52 -0300

    Trabalhando com multiplos ambientes e profiles

--- src/main/java/com/eduardo/vendas/config/MinhaConfiguration.java
+++ src/main/java/com/eduardo/vendas/config/MinhaConfiguration.java
@@ -1,9 +1,18 @@
 package com.eduardo.vendas.config;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("development")
 public class MinhaConfiguration {
 
+    @Bean
+    public CommandLineRunner executar(){
+        return args -> {
+            System.out.println("RODANDO AMBIENTE DE DESENVOLVIMENTO.");
+        };
+    }
 }
