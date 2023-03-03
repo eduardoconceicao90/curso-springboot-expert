861c3eb1df08a8ae0607772b8466f215684af8ef
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-03-03 15:30:20 -0300

    Salvando e recuperando clientes

--- src/main/java/com/eduardo/vendas/VendasApplication.java
+++ src/main/java/com/eduardo/vendas/VendasApplication.java
@@ -1,15 +1,32 @@
 package com.eduardo.vendas;
 
+import com.eduardo.vendas.domain.Cliente;
+import com.eduardo.vendas.repository.ClienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 public class VendasApplication {
+
+	@Bean
+	public CommandLineRunner init(@Autowired ClienteRepository clienteRepository){
+		return args -> {
+			clienteRepository.salvar(new Cliente("Eduardo"));
+			clienteRepository.salvar(new Cliente("Telma"));
+
+			List<Cliente> todosClientes = clienteRepository.obterTodos();
+			todosClientes.forEach(System.out::println);
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
