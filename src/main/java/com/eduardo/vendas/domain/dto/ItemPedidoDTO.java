c9e728cfc96b8af44b07f89eff02bf27265b4e5a
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-03-08 14:56:04 -0300

    Utilizando Lombok

--- src/main/java/com/eduardo/vendas/domain/dto/ItemPedidoDTO.java
+++ src/main/java/com/eduardo/vendas/domain/dto/ItemPedidoDTO.java
@@ -1,5 +1,12 @@
 package com.eduardo.vendas.domain.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class ItemPedidoDTO {
 
     private Integer produto;
