80d4d474c294ed6f7bbe7c9c3b853f7c1fdcf354
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-03-10 11:19:30 -0300

    Validando a entidade pedido

--- src/main/java/com/eduardo/vendas/controller/PedidoController.java
+++ src/main/java/com/eduardo/vendas/controller/PedidoController.java
@@ -14,6 +14,7 @@ import org.springframework.util.CollectionUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import javax.validation.Valid;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.List;
@@ -28,7 +29,7 @@ public class PedidoController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Integer save(@RequestBody PedidoDTO pedidoDTO){
+    public Integer save(@Valid @RequestBody PedidoDTO pedidoDTO){
         Pedido pedido = pedidoService.salvar(pedidoDTO);
         return pedido.getId();
     }
