3a5b49cdcdabae6af855a60ecfa85fbbfbde27c9
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-03-11 21:04:59 -0300

    Implementação do UserDetailsService

--- src/main/java/com/eduardo/vendas/config/SecurityConfig.java
+++ src/main/java/com/eduardo/vendas/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.eduardo.vendas.config;
 
+import com.eduardo.vendas.service.impl.UsuarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,31 +13,34 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    public PasswordEncoder passwordEncoder() {
+    @Autowired
+    private UsuarioServiceImpl usuarioService;
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .passwordEncoder(passwordEncoder())
-                .withUser("Fulano")
-                .password(passwordEncoder().encode("123"))
-                .roles("USER", "ADMIN");
+        auth
+                .userDetailsService(usuarioService)
+                .passwordEncoder(passwordEncoder());
     }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    protected void configure( HttpSecurity http ) throws Exception {
         http
                 .csrf().disable()
                 .authorizeRequests()
                     .antMatchers("/api/clientes/**")
                         .hasAnyRole("USER", "ADMIN")
+                    .antMatchers("/api/pedidos/**")
+                        .hasAnyRole("USER", "ADMIN")
                     .antMatchers("/api/produtos/**")
                         .hasRole("ADMIN")
-                    .antMatchers("api/pedidos/**")
-                        .hasAnyRole("USER", "ADMIN")
                 .and()
                     .httpBasic();
     }
+
 }
