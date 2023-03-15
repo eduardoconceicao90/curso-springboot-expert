802b8b5e4d22c088f8547baff6194ed9ae4fb90e
Eduardo Conceição <eduardosaconceicao@gmail.com>
2023-03-14 21:39:59 -0300

    Configurando o Swagger

---
+++ src/main/java/com/eduardo/vendas/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+package com.eduardo.vendas.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.eduardo.vendas.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Vendas API")
+                .description("API do curso Spring Boot Expert")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+
+    private Contact contact(){
+        return new Contact(
+                "Eduardo Conceição",
+                "https://github.com/eduardoconceicao90?tab=repositories",
+                "eduardosaconceicao@gmail.com");
+    }
+}
